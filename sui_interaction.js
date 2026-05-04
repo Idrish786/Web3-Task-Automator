cc127eb467dc6043663ad6a3e4e58c7e101ff44d
cryptoid <166424672+Idrish786@users.noreply.github.com>
2026-05-04 14:30:38 +0530

    Update sui_interaction.js

--- sui_interaction.js
+++ sui_interaction.js
@@ -3,3 +3,6 @@ const { JsonRpcProvider, Connection } = require('@mysten/sui.js');
 const config = require('./config.js');
 
 // Initialize SUI Network Provider connecting to Mainnet
+
+const connection = new Connection({ fullnode: config.networks.sui.rpc });
+​const provider = new JsonRpcProvider(connection);
