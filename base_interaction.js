f9ada116abdb7fe927a2f31a47c87444224e3e9e
cryptoid <166424672+Idrish786@users.noreply.github.com>
2026-05-05 09:55:36 +0530

    Update base_interaction.js

--- base_interaction.js
+++ base_interaction.js
@@ -5,3 +5,9 @@ const config = require('./config.js');
 // Initialize Base Network RPC Connection
 
 const baseProvider = new ethers.JsonRpcProvider(config.networks.base.rpc);
+
+async function checkBaseGas() {
+
+  try {
+
+    const feeData = await baseProvider.getFeeData();
