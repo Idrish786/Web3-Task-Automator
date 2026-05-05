27b77fb5f763f21524bb6930aa9dec402f041977
cryptoid <166424672+Idrish786@users.noreply.github.com>
2026-05-05 09:54:56 +0530

    Update base_interaction.js

--- base_interaction.js
+++ base_interaction.js
@@ -1,3 +1,7 @@
 const { ethers } = require('ethers');
 
 const config = require('./config.js');
+
+// Initialize Base Network RPC Connection
+
+const baseProvider = new ethers.JsonRpcProvider(config.networks.base.rpc);
