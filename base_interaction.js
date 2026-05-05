89824b4d581978a3ff27b4a342810e5373984556
cryptoid <166424672+Idrish786@users.noreply.github.com>
2026-05-05 09:56:15 +0530

    Update base_interaction.js

--- base_interaction.js
+++ base_interaction.js
@@ -11,3 +11,11 @@ async function checkBaseGas() {
   try {
 
     const feeData = await baseProvider.getFeeData();
+
+  const gasPrice = ethers.formatUnits(feeData.gasPrice, 'gwei');
+
+    console.log("Current Base Network Gas:", gasPrice, "Gwei");
+
+    return parseFloat(gasPrice);
+
+  } catch (err) {
