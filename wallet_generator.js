cfad0dcc5efa29c82c0e7ef1a15ae8f9ec667e45
cryptoid <166424672+Idrish786@users.noreply.github.com>
2026-05-09 17:59:07 +0530

    Update wallet_generator.js

--- wallet_generator.js
+++ wallet_generator.js
@@ -9,3 +9,9 @@ function generateNewWallet() {
 const wallet = ethers.Wallet.createRandom();
   
 console.log("New Wallet Successfully Created:", wallet.address);
+
+return { 
+    address: wallet.address, 
+    privateKey: wallet.privateKey, 
+    phrase: wallet.mnemonic.phrase 
+  };
