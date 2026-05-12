550c74334b685f1ea1618af431eecf897ec0284a
cryptoid <166424672+Idrish786@users.noreply.github.com>
2026-05-12 20:18:32 +0530

    Update token_swapper.js

--- token_swapper.js
+++ token_swapper.js
@@ -3,3 +3,5 @@ const { ethers } = require('ethers');
 const config = require('./config.js');
 
 // Decentralized Exchange Swap Execution Module
+
+const UNISWAP_ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
