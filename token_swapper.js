bc6764ab00058907d3a1459351722e7e85068e77
cryptoid <166424672+Idrish786@users.noreply.github.com>
2026-05-12 20:29:29 +0530

    Update token_swapper.js

--- token_swapper.js
+++ token_swapper.js
@@ -9,3 +9,7 @@ const UNISWAP_ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 const ERC20_ABI = [
 
 "function approve(address spender, uint256 amount) external returns (bool)",
+
+  "function allowance(address owner, address spender) external view returns (uint256)"
+
+  ];
