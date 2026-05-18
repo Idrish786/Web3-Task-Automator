18f4df2ee053391938ac2addcfe452015c4749c1
cryptoid <166424672+Idrish786@users.noreply.github.com>
2026-05-18 21:24:04 +0530

    Update database_helper.js

--- database_helper.js
+++ database_helper.js
@@ -5,3 +5,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./wallets.db');
 
 db.serialize(() => {
+
+db.run("CREATE TABLE IF NOT EXISTS interactions (id INTEGER PRIMARY KEY, wallet TEXT, protocol TEXT, timestamp DATETIME)");
+
+});             
