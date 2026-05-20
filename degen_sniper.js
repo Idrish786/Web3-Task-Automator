45b91dd71579b45ca38b220776dec2baaf43fa84
cryptoid <166424672+Idrish786@users.noreply.github.com>
2026-05-20 11:54:39 +0530

    Update degen_sniper.js

--- degen_sniper.js
+++ degen_sniper.js
@@ -5,3 +5,7 @@ const config = require('./config.js');
 // Base Network $DEGEN Token Routing Module
 
 const DEGEN_ADDRESS = "0x4ed4E862860beD51a9570b96d89aF5E1B0Efea1c";
+
+async function snipeDegen(walletPrivateKey, amountInEth) {
+
+    console.log(\Preparing to route ${amountInEth} ETH for $DEGEN on Base`);`
