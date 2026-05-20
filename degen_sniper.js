fc4c3af9eed1e6ff2fd40bede903e3f9fcbc3cdd
cryptoid <166424672+Idrish786@users.noreply.github.com>
2026-05-20 12:00:29 +0530

    Update degen_sniper.js

--- degen_sniper.js
+++ degen_sniper.js
@@ -9,3 +9,7 @@ const DEGEN_ADDRESS = "0x4ed4E862860beD51a9570b96d89aF5E1B0Efea1c";
 async function snipeDegen(walletPrivateKey, amountInEth) {
 
     console.log(\Preparing to route ${amountInEth} ETH for $DEGEN on Base`);`
+
+const provider = new ethers.JsonRpcProvider(config.networks.base.rpc);
+
+    const signer = new ethers.Wallet(walletPrivateKey, provider);
