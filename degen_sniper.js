04d4fadba6e3cee5ff21a0055894ec4e9a6f462e
cryptoid <166424672+Idrish786@users.noreply.github.com>
2026-05-20 11:49:40 +0530

    Update degen_sniper.js

--- degen_sniper.js
+++ degen_sniper.js
@@ -2,3 +2,6 @@ const { ethers } = require('ethers');
 
 const config = require('./config.js');
 
+// Base Network $DEGEN Token Routing Module
+
+const DEGEN_ADDRESS = "0x4ed4E862860beD51a9570b96d89aF5E1B0Efea1c";
