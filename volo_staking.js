4d945813527d6116f8a45268872bfdced5e20cbb
cryptoid <166424672+Idrish786@users.noreply.github.com>
2026-05-21 10:52:19 +0530

    Update volo_staking.js

--- volo_staking.js
+++ volo_staking.js
@@ -3,3 +3,7 @@ const { JsonRpcProvider, Connection } = require('@mysten/sui.js');
 const config = require('./config.js');
 
 // Automated Volo Liquid Staking on SUI Network
+
+const VOLO_MAINNET_POOL = "0x7fa2292fa99803c3e...volo_pool";
+
+async function stakeSuiToVolo(walletAddress, amountToStake) {
